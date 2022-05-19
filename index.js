ea6613120d5ea19979584808ea20d8fc030605b2
John Soto <jsoto1@ufl.edu>
2022-05-18 22:45:37 -0400

    added function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function greet() {
-    var sidesStr = prompt('What is your name?')
+    var name = prompt('What is your name?');
+    alert('Hello ' + name);
+
+    var age = prompt('How old are you?');
+    console.log(parseInt(age));
+
+    confirm("Have you had a birthday yet this year?");
+
 }
