3881b02285693a9863d48ced97b8162baf7e22b5
John Soto <jsoto1@ufl.edu>
2022-05-19 21:40:57 -0400

    added if else

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function greet() {
     var name = prompt('What is your name?');
     alert('Hello ' + name);
-
     var age = prompt('How old are you?');
-    console.log(parseInt(age));
-
-    var birthday = confirm("Have you had a birthday yet this year?");
+    var age = (parseInt(age));
+    var birthday = confirm('Have you had a birthday yet this year?');
     var year = new Date().getFullYear();
     var birthdayYear = year - age;
+    var birthdayYearPast = year - (age + 1);
+    if (birthday){
+        alert('You were born in ' + birthdayYear);
+    } else {
+        alert('You were born in ' + birthdayYearPast);
+    }
 }
