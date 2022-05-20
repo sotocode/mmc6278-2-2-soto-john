71650093f09b453bc218267f56606afeb477c547
John Soto <jsoto1@ufl.edu>
2022-05-20 17:20:04 -0400

    made corrections

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function greet() {
     var name = prompt('What is your name?');
     alert('Hello ' + name);
     var age = prompt('How old are you?');
-    var age = (parseInt(age));
+    age = (parseInt(age));
     var birthday = confirm('Have you had a birthday yet this year?');
     var year = new Date().getFullYear();
     var birthdayYear = year - age;
