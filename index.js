76beac17f268ab299a28f9381f339e66c767345d
John Soto <jsoto1@ufl.edu>
2022-05-19 20:59:29 -0400

    added year

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function greet() {
     var age = prompt('How old are you?');
     console.log(parseInt(age));
 
-    confirm("Have you had a birthday yet this year?");
-
+    var birthday = confirm("Have you had a birthday yet this year?");
+    var year = new Date().getFullYear();
+    var birthdayYear = year - age;
 }
